d67806d71224cb2ea3ad454e9e3376eb7d57efe5
aidatahirbegovic <aidatahirbeg28@hotmail.com>
2020-09-04 10:23:14 +0200

    first line

---
+++ Elektrotek/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Elektrotek.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Sayfamizi anlatacak kisim";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "iletisim";
+
+            return View();
+        }
+    }
+}
